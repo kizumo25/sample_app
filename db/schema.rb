c9cf8c9164e5b34301452f89132863e2923ce837
kizumo25 <kizumo25@gmail.com>
2021-02-18 01:20:51 +0000

    Add account activation

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210216080429) do
+ActiveRecord::Schema.define(version: 20210217013327) do
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "password_digest"
-    t.string "remember_digest"
-    t.boolean "admin", default: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-  end
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'tring' for column 'activation_digest'
 
 end
